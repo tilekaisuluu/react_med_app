e018c0c5e1e234e28baf95458f0c9110d5f3960c
Aisuluu <aisuluuisabekova@gmail.com>
2020-02-13 18:46:20 +0600

    react search function for medication app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const drugs = [
+  "Ibuprofen",
+  "Aspirin",
+  "Bad drug",
+  "Abc",
+  "Xyz",
+];
+function App() {
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const [searchResults, setSearchResults] = React.useState([]);
+  const handleChange = e => {
+    setSearchTerm(e.target.value);
+  };
+  React.useEffect(() => {
+    const results = drugs.filter(drug =>
+      drug.toLowerCase().includes(searchTerm)
+    );
+    setSearchResults(results);
+  }, [searchTerm]);
+  return (
+    <div className="App">
+      <input
+        type="text"
+        placeholder="Search"
+        value={searchTerm}
+        onChange={handleChange}
+      />
+      
+
+      <ul class="list-group">
+        {searchResults.map(item => (
+          <li class="list-group-item disabled">{item}</li>
+        ))}
+      </ul>
+      
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
