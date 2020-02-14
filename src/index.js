d285bdab816fb29651ebd24bf7af3fe15b54ab01
Aisuluu <aisuluuisabekova@gmail.com>
2020-02-14 20:43:28 +0600

    add react modal

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Info from './components/info';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
+    /* create a list of drugs  */
 const drugs = [
   "Ibuprofen",
   "Aspirin",
@@ -12,31 +13,53 @@ const drugs = [
   "Xyz",
 ];
 function App() {
+
+
+  /*  searchTerm is value of input which save the data using function setSearchTerm */
+
   const [searchTerm, setSearchTerm] = React.useState("");
+
+  /*  searchResults is results */
+
   const [searchResults, setSearchResults] = React.useState([]);
   const handleChange = e => {
     setSearchTerm(e.target.value);
   };
+ 
+
+
+  /* effect hook is used to executes when method gets changed */
+
+
   React.useEffect(() => {
+      
+    /* filter method returns a new array of list according to the searchTerm */
     const results = drugs.filter(drug =>
       drug.toLowerCase().includes(searchTerm)
     );
     setSearchResults(results);
   }, [searchTerm]);
+
+
+
   return (
     <div className="App">
+        <Info>
+            <button>x</button>
+            Hello
+        </Info>
       <input
         type="text"
         placeholder="Search"
-        value={searchTerm}
-        onChange={handleChange}
       />
-      
+      <button onClick = {handleChange}>Submit</button>
 
-      <ul class="list-group">
+      <ul className="list-group">
         {searchResults.map(item => (
-          <li class="list-group-item disabled">{item}</li>
+          <li key={item.toString()} className ="list-group-item disabled">{item}</li>
         ))}
+        <button>More Info</button>
+
       </ul>
       
     </div>
