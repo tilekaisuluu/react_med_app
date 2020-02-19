f093b31acb624a1085e34e12d172a6e9ad7cd036
Aisuluu <aisuluuisabekova@gmail.com>
2020-02-19 19:24:56 +0600

    modal window

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const drugsData = {
             "date_on_market": "01/10/1990",
             "side_effects": [
                 {
-                    "side_effect_name":"headache",
+                    "side_effect_name": "headache",
                     "date_discovered": "01/01/2000"
                 },
                 {
@@ -28,7 +28,7 @@ const drugsData = {
         {
             "medication_name": "xyz",
             "manufacturer": "another company",
-            "date_on_market":  "02/10/2005",
+            "date_on_market": "02/10/2005",
             "side_effects": [
                 {
                     "side_effect_name": "headache",
@@ -41,36 +41,91 @@ const drugsData = {
             ]
         }
     ]
- }
- 
-
-const names = drugsData.medications.map(({medication_name}) => medication_name);
-
- console.log(names);
-
+}
 
 
 function App() {
-  // searchTerm save the input in every onChange event 
-  const [searchTerm, setSearchTerm] = React.useState("");
-  // state searchResults used to set the search result
-  const [searchResults, setSearchResults] = React.useState(names);
-  // handleChange method takes event as argument and sets the value to the searchTerm state using SetSearchTerm, provided in React.useState method 
-  const handleChange = e => {
-    setSearchTerm(e.target.value);
-  };
+    // searchTerm save the input in every onChange event 
+    const [searchTerm, setSearchTerm] = React.useState("");
+    // state searchResults used to set the search result
+    const [searchResults, setSearchResults] = React.useState([]);
+    // handleChange method takes event as argument and sets the value to the searchTerm state using SetSearchTerm, provided in React.useState method 
+    const handleChange = e => {
+        setSearchTerm(e.target.value);
+    };
 
+    const [isModalOpen, setIsModalOpen] = React.useState(false);
+
+
+
+    // 
+    const onButtonClick = React.useCallback((e) => {
+        // by default onsubmit is request thats why we need event prevent Default
+        e.preventDefault();
+        // json data
+        const drugsDataStr = JSON.stringify(drugsData);
+        const parsedDrugsData = JSON.parse(drugsDataStr)
+        // results element filter drugs and converts string  to lowercase with method toLowerCase 
+        const results = parsedDrugsData.medications.filter(({ medication_name }) => medication_name.toLowerCase().includes(searchTerm));
+        // filtered list is set on the searchResults using setSearchResults
+        setSearchResults(results);
+        // whenever in searchTerm array something changed the first argument of React.useCallback hook is executes
+    }, [searchTerm]);
+
+
+
+
+
+
+    return (
+        // onSubmit attribute call onButtonClick when the form is submitted
+        <form onSubmit={onButtonClick} className="App">
+            {/* input field where onChange attribute call handleChange method  */}
+            <input onChange={handleChange} type="text" placeholder="Search" />
+            <button type="submit">Submit</button>
+            {/* searchResults.map function iterates through all searchResults and render it inside ul element, toString converts elements to string, return a li element for each item*/}
+            <ul className="list-group">
+            {searchResults.map(({ medication_name }) => (
+            <li key={medication_name.toString()} className="list-group-item disabled">{medication_name}</li>)
+            )}
+            <button onClick={() => setIsModalOpen(true)}>More Info</button>
+            {isModalOpen && (
+            <div>
+                <p>
+                    It's a modal
+              
+                </p>
+                <button onClick={() => setIsModalOpen(false)}>CLOSE MODAL</button>
+            </div>
+            )}
+            </ul>
+           {/*<pre>{JSON.stringify(searchResults, null, 2)}</pre> */} 
+        </form>
+    );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
+
+
+
+/* toggle is the switch component
 const ToggleContent = ({ toggle, content }) => {
     const [isShown, setIsShown] = React.useState(false);
+
     const hide = () => setIsShown(false);
+
     const show = () => setIsShown(true)
+
+    // React.Fragman let use multiple elements without adding nodes to the DOM
     return (
         <React.Fragment>
           {toggle(show)}
           {isShown && content(hide)}
         </React.Fragment>
       );
-    }    
+    }
 const Modal = ({ children }) => (
     ReactDOM.createPortal(
     <div className="modal">
@@ -81,36 +136,7 @@ const Modal = ({ children }) => (
         );
 
 
- 
-
-  // 
-  const onButtonClick = React.useCallback((e) => {
-    // by default onsubmit is request thats why we need event prevent Default
-    e.preventDefault();
-    // results element filter drugs and converts string  to lowercase with method toLowerCase 
-    const results = names.filter(name => name.toLowerCase().includes(searchTerm)); 
-    // filtered list is set on the searchResults using setSearchResults
-    setSearchResults(results);
-    // whenever in searchTerm array something changed the first argument of React.useCallback hook is executes
-}, [searchTerm]);
-
-
-
-
-
-
-  return (
-      // onSubmit attribute call onButtonClick when the form is submitted
-    <form onSubmit = {onButtonClick} className="App">
-        {/* input field where onChange attribute call handleChange method  */}
-      <input onChange={handleChange} type="text" placeholder="Search"/>
-      <button type="submit">Submit</button>
-      {/* searchResults.map function iterates through all searchResults and render it inside ul element, toString converts elements to string, return a li element for each item*/}
-      <ul 
-      
-       
-      className="list-group">
-          <ToggleContent
+<ToggleContent
           toggle={show => <button onClick={show}>Open</button>}
           content={hide => (
             <Modal>
@@ -119,17 +145,5 @@ const Modal = ({ children }) => (
             </Modal>
           )}
         />
-           {searchResults.map(item => (
-          
-      <li key={item.toString()} className ="list-group-item disabled">{item}</li>)
-   
-      )}
-      </ul>
-    </form>
 
-  );
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
-
+*/
